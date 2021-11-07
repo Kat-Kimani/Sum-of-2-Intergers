409f7862db5709cd1a3adbf64561f47fc5c2222c
Catherine Kimani <catherine.wachu@strathmore.edu>
2021-11-07 07:13:28 +0300

    Add project files.

---
+++ 2intergersRepeat.cpp
@@ -0,0 +1,26 @@
+//Sum of Two Numbers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int num1, num2, sum;
+
+
+	cout << "Welcome to our Basic Caclulator";
+	cout << "......................\n";
+	cout << "Please input first number : ";
+	cin >> num1;
+	cout << "Please input second number : ";
+	cin >> num2;
+	sum = num1 + num2;
+
+	cout << "sum of the two numbers is : " << sum;
+	cout << " \n\n\n\n\n\n ";
+
+
+
+
+	return 0;
+}
